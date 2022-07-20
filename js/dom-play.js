73b0b5b5940d3cf65761798c33d2ad2ce5cd5632
hsojeel <Joshua.S.Lee@seattlecolleges.edu>
2022-07-20 09:39:29 -0700

    update js

--- js/dom-play.js
+++ js/dom-play.js
@@ -4,18 +4,37 @@ function myAlert(){
     alert("I'm clicked!");
 }
 
-let spans = document.querySelectorAll("#play span");
+const spans = document.querySelectorAll("#play span");
 
 console.log(spans);
 
 for(const mySpan of spans){
-    mySpan.addEventListener("click",myAlert);
+    //mySpan.addEventListener("click",myAlert);
+
+    mySpan.addEventListener("click", function(ev){
+        //alert("The current actor is " + mySpan.dataset.actor);
+        highlightActor(mySpan.dataset.actor);
+    });
 }
+
+
+function highlightActor(actor){
+
+    for(const mySpan of spans){
+
+        if(actor == mySpan.dataset.actor){ //current actor, highlight
+            mySpan.style.backgroundColor='lightblue';
+        }else{ //other actor, background white
+            mySpan.style.backgroundColor='white';
+        }
+    } 
+}
+
 
 
 function highlight(el){
 
-    if(el.style.backgroundColor == 'lightseagreen'){      
+    if(el.style.backgroundColor == 'lightblue'){      
         el.style.backgroundColor='white';
     }else{      
         el.style.backgroundColor='lightseagreen';
